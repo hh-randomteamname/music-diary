498d07a993e0b0e99864d8c71b5be5ea8e8237da
Carter Strickland <carter.strickland57@gmail.com>
2021-09-11 17:13:00 -0500

    Confirmed working starter code
    Co-authored-by: BraedenSmith29 <BraedenSmith29@users.noreply.github.com>

--- music-diary/src/App.js
+++ music-diary/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Or don't. Do whatever you want.
+          Or don't. Do whatever you want. Bananas.
         </p>
         <a
           className="App-link"
